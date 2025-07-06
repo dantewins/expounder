2b182ac46e872488f8341ed5199bc64fbec8d247
Azurium-js <kimdanny0603@gmail.com>
2025-07-06 16:02:50 -0400

    cleaned up file tree x2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,3 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-
 export default async function DashboardPage() {
 
     return (
